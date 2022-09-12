198bde14f99b9c128bf814221a66e3890c08bf2d
Semen-1989 <orislav1989@gmail.com>
2022-09-12 19:45:38 +0300

    fixed tests

--- Сalculator/Tests/Calculator_test.py
+++ Сalculator/Tests/Calculator_test.py
@@ -3,16 +3,16 @@ from Сalculator.First.Сalculator import calculator_test
 
 class MyTestCase(unittest.TestCase):
     def test_plus(self):
-        self.assertEqual(calculator_test('2+2'), 4)  # add assertion here
+        self.assertEqual(calculator_test('2+2'), ['will be:', 4])  # add assertion here
 
     def test_minus(self):
-        self.assertEqual(calculator_test('71-98'), -27)
+        self.assertEqual(calculator_test('71-98'), ['will be:', -27])
 
     def test_multi(self):
-        self.assertEqual(calculator_test('1234*98'), 120932)
+        self.assertEqual(calculator_test('1234*98'), ['will be:', 120932])
 
     def test_divide(self):
-        self.assertEqual(calculator_test('14/3'), 4.666666666666667)
+        self.assertEqual(calculator_test('14/3'), ['will be:', 4.666666666666667])
 
     def test_SyntaxError(self):
         self.assertEqual(calculator_test('09 * 7' or "9***4"),
